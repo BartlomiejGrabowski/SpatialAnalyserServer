461563fcdf86eb680446ae8197c032ebb6d0a27c
Bartlomiej Grabowski <bartlomiej.grabowski@tlen.pl>
2012-04-22 21:07:09 +0200

    Added get_best_db function.

--- SpatialAnalyserServer/database/performance/FindDB.py
+++ SpatialAnalyserServer/database/performance/FindDB.py
@@ -2,6 +2,7 @@ import Queue
 import threading
 import ping
 import socket
+import operator
 
 
 
@@ -33,27 +34,34 @@ class FindDB(threading.Thread):
     def get_db_delay(self):
         hosts = ["212.77.100.101", "173.194.70.139", "213.180.141.140",
                 "127.0.0.1", "195.93.178.6"]
-        #times = {}
+
         #spawn a pool of threads, and pass them queue instance 
         for i in range(5):
             t = FindDB(ips, times)
             t.setDaemon(True)
             t.start()
         
-            #populate queue with data   
+        #populate queue with data   
         for host in hosts:
             ips.put(host)
      
         #wait on the queue until everything has been processed     
         ips.join()
         
-        for key in times.iterkeys():
-            print key, times[key]
+        return times
 
+    def get_best_db(self):
+        #Function returns ip address with minimum delay value.
+        delay_list = self.get_db_delay()
+        #sort dictionary from get_db_delay() function
+        sorted_list = sorted(delay_list.iteritems(), key=operator.itemgetter(1))
+        #return first element of list (min delay value).
+        return sorted_list.__getitem__(0)
+        
 ips = Queue.Queue()   
 times = {}
 
 obj = FindDB(ips, times)
-obj.get_db_delay()
+obj.get_best_db()
 
 
