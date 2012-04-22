783bf33ee3211dde5862c542a6c437c27e459df3
Bartlomiej Grabowski <bartlomiej.grabowski@tlen.pl>
2012-04-22 21:57:04 +0200

    Added idl interface DB::Shp

---
+++ SpatialAnalyserServer/interfaces/db/Shp_i.py
@@ -0,0 +1,21 @@
+'''
+Created on Apr 22, 2012
+
+@author: bartek
+'''
+
+import idls.db_shp.DB
+import idls.db_shp.DB__POA
+
+
+class Shp_i(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
--- SpatialAnalyserServer/src/server/main/Server.py
+++ SpatialAnalyserServer/src/server/main/Server.py
@@ -3,9 +3,24 @@ Created on Mar 31, 2012
 
 @author: bartek
 '''
-import database.postgres.Basic
-from database.performance import FindDB
+import sys
+from omniORB import CORBA, PortableServer
+import Example, Example__POA
 
-if __name__ == '__main__':
-    p = FindDB()
-    p.pingTiming()
+class Echo_i (Example__POA.Echo):
+    def echoString(self, mesg):
+        print("echoString() called with message:", mesg)
+        return mesg
+
+orb = CORBA.ORB_init(sys.argv, CORBA.ORB_ID)
+poa = orb.resolve_initial_references("RootPOA")
+
+ei = Echo_i()
+eo = ei._this()
+
+print orb.object_to_string(eo);
+
+poaManager = poa._get_the_POAManager()
+poaManager.activate()
+
+orb.run();
